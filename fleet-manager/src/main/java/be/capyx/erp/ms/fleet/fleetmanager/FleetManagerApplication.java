04c7eccd50f9a86f62eac3d7310d801a3409ac00
Akrem AYADI <ayadi.akrem@gmail.com>
2019-03-06 22:20:12 +0100

    Add Hystrix to Fleet-manager

--- fleet-manager/src/main/java/be/capyx/erp/ms/fleet/fleetmanager/FleetManagerApplication.java
+++ fleet-manager/src/main/java/be/capyx/erp/ms/fleet/fleetmanager/FleetManagerApplication.java
@@ -3,7 +3,11 @@ package be.capyx.erp.ms.fleet.fleetmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrix
+@EnableHystrixDashboard
 @EnableEurekaClient
 @SpringBootApplication
 public class FleetManagerApplication {
--- fleet-manager/src/main/java/be/capyx/erp/ms/fleet/fleetmanager/controller/FleetController.java
+++ fleet-manager/src/main/java/be/capyx/erp/ms/fleet/fleetmanager/controller/FleetController.java
@@ -2,6 +2,8 @@ package be.capyx.erp.ms.fleet.fleetmanager.controller;
 
 
 import be.capyx.erp.ms.fleet.fleetmanager.model.Car;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/fleet")
 public class FleetController {
 
+
+    @HystrixCommand(fallbackMethod = "fallBackRetrieveCar", commandKey = "car_1",
+            groupKey = "fleet_1")
     @GetMapping("/{employee}")
     public Car find(@PathVariable String employee) {
+        if(RandomUtils.nextBoolean()){
+            throw new RuntimeException("Unexpected Exception");
+        }
+
         if (employee.equals("12345")) {
             return new Car("1 KFS 544");
         }
         return new Car("1 PTT 999");
+    }
+
+    public Car fallBackRetrieveCar(String employee){
+       return new Car("X-XXX-XXX");
     }
 
 }
